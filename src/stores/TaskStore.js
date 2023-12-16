250bc05f838bd1f48f93cd9c056fba471179110d
vida <m.vidamohammadi@gmail.com>
2023-12-16 10:06:18 -0800

    create first state using defineStore

---
+++ src/stores/TaskStore.js
@@ -0,0 +1,15 @@
+import { defineStore } from "pinia";
+
+export const useTaskStore = defineStore("taskStore", {
+    state: () => ({
+        tasks: [
+            { id: 1, title: "buy some milk", isFav: false },
+            { id: 2, title: "play Gloomhaven", isFav: true }
+        ]
+    }),
+    actions: {
+        addTask(task) {
+            this.tasks.push(task);
+        }
+    }
+})
