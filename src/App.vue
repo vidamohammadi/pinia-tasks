a4ce5d35c7472302c4c16c1d1a78f529837d13b2
vida <m.vidamohammadi@gmail.com>
2023-12-16 10:36:29 -0800

    use state in component and changes in styles

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,25 @@
       <img src="./assets/pinia-logo.svg" alt="pinia logo">
       <h1>Pinia Tasks</h1>
     </header>
+
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks" :key="task.id">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
   </main>
 </template>
 
-<style scoped>
-</style>
+<script>
+import { useTaskStore } from './stores/TaskStore';
+  import TaskDetails from './components/TaskDetails.vue';
+export default {
+    components: {
+      TaskDetails
+    },
+    setup() {
+      const taskStore = useTaskStore();
+      return { taskStore };
+    }
+  }
+</script>
